608e82c48633ad0ac5ecbf3438ac48093469ee69
abhi1994j <abhibaban4@gmail.com>
2025-04-30 01:36:06 +0530

    grocery bud

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import { useEffect, useState } from "react";
 import {  toast } from 'react-toastify';
+import { FaRegTrashAlt } from "react-icons/fa";
 
 function App() {
-      const [item, setItem] = useState('');
-      const [grocery , setGrocery] = useState(()=>localStorage.getItem("grocery")? JSON.parse(localStorage.getItem("grocery")):[])
+      const [item, setItem] = useState({text:"" , check:false});
+      const [grocery , setGrocery] = useState(()=>localStorage.getItem("grocery")? JSON.parse(localStorage.getItem("grocery")):[]);
 
       function handleSubmit(e){
         e.preventDefault();
-        if(!item.trim()){
+        if(!item.text.trim()){
          toast.warn("Please Enter a Grocery item", {position: "top-center",})
-        }else if(!/^[A-Za-z]+$/.test(item)){
+        }else if(!/^[A-Za-z]+$/.test(item.text)){
           toast.error("Please Enter a valid Grocery item", {position: "top-center",})
-        }else if(grocery.includes(item)){
+        }else if(grocery.some(ele => ele.text.toLowerCase() === item.text.toLowerCase())){
           toast.warn("Item is already exist", {position: "top-center",})
         }
         else{
-          const updatedGrocery = [...grocery , item]
+          const updatedGrocery = [...grocery , {text:item.text , check:item.check}]
             setGrocery(updatedGrocery);
             toast.success("Item added Successfully", {position: "top-center",})
             
         }
-        setItem('')
+        setItem({ text: "", check: false });
       }
+
       useEffect(()=>{
         localStorage.setItem("grocery" , JSON.stringify(grocery));
       } , [grocery])
+
       function handleDelete(id){
+          toast.error("Item Deleted", {position: "top-center",});
           const filteredGrocery = grocery.filter((grocery, index)=>index !== id)
           return setGrocery(filteredGrocery);
       }
+
+    function handleCheck(id){
+      console.log(id);
+      const updateGrocery = grocery.map((ele, index)=>index === id ? {...ele , check:!ele.check} : ele)
+      setGrocery(updateGrocery)
+    }
+
+    function handleChange(e){
+      console.log(e);
+      const {name , value} = e.target ;
+      setItem({...item , [name]:value})
+    }
 
   return (
     <>
@@ -38,9 +54,9 @@ function App() {
           <form className="flex justify-center items-center mb-4 w-full" onSubmit={handleSubmit}>
             <input
               type="text"
-              name="item"
-              value={item}
-              onChange={(e)=>setItem(e.target.value)}
+              name="text"
+              value={item.text}
+              onChange={handleChange}
               className="py-1 px-3 bg-gray-100 md:text-lg text-sm"
               placeholder="Enter your Grocery item"
             />
@@ -51,10 +67,10 @@ function App() {
           {grocery.map((ele ,index)=>
               <div className="cards w-full flex justify-between items-center h-auto mb-2" key={index}>
               <div className="flex gap-4">
-                <input type="checkbox" />
-                <p className="text-sm md:text-lg text-gray-500 ">{ele}</p>
+                <input type="checkbox" name="check" checked={ele.check} onChange={()=>handleCheck(index)}/>
+                <p className={`text-sm md:text-lg  text-gray-500 ${ ele.check? "line-through" : ""} `}>{ele.text}</p>
               </div>
-              <button onClick={()=>handleDelete(index)} className="text-white bg-red-500 md:text-lg text-sm hover:bg-red-600 py-1 md:py-2 px-3 rounded-md text-center md:px-4">Delete</button>
+              <button onClick={()=>handleDelete(index)} className="text-white bg-red-500 md:text-lg text-sm hover:bg-red-700 py-1 md:py-2 px-2 rounded-md text-center"><FaRegTrashAlt/></button>
             </div>
           )}
           
